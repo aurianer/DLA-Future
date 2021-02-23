5ae6493066923a7bf71fb78f88e18bdb881022b1
Alberto Invernizzi <invernizzi@cscs.ch>
2021-02-23 15:47:17 +0100

    refactor cholesky
    adapt to new Panel API with Size/Index

--- include/dlaf/factorization/cholesky/mc.h
+++ include/dlaf/factorization/cholesky/mc.h
@@ -14,8 +14,6 @@
 #include <hpx/include/threads.hpp>
 #include <hpx/include/util.hpp>
 
-#include <unordered_map>
-
 #include "dlaf/blas_tile.h"
 #include "dlaf/common/index2d.h"
 #include "dlaf/common/pipeline.h"
@@ -28,6 +26,7 @@
 #include "dlaf/factorization/cholesky/api.h"
 #include "dlaf/lapack_tile.h"
 #include "dlaf/matrix/distribution.h"
+#include "dlaf/matrix/panel.h"
 
 #include "dlaf/memory/memory_view.h"
 #include "dlaf/util_matrix.h"
@@ -113,10 +112,26 @@ void Cholesky<Backend::MC, Device::CPU, T>::call_L(Matrix<T, Device::CPU>& mat_a
 }
 
 template <class T>
+struct RoundRobin {
+  template <class... Args>
+  RoundRobin(std::size_t n, Args&&... args) : next_index_(0) {
+    for (std::size_t i = 0; i < n; ++i)
+      pool_.emplace_back(std::forward<Args>(args)...);
+  }
+
+  T& next_resource() {
+    auto idx = (next_index_ + 1) % pool_.size();
+    std::swap(idx, next_index_);
+    return pool_[idx];
+  }
+
+  std::size_t next_index_;
+  std::vector<T> pool_;
+};
+
+template <class T>
 void Cholesky<Backend::MC, Device::CPU, T>::call_L(comm::CommunicatorGrid grid,
                                                    Matrix<T, Device::CPU>& mat_a) {
-  using ConstTileType = typename Matrix<T, Device::CPU>::ConstTileType;
-
   auto executor_hp = dlaf::getHpExecutor<Backend::MC>();
   auto executor_np = dlaf::getNpExecutor<Backend::MC>();
   auto executor_mpi = dlaf::getMPIExecutor<Backend::MC>();
@@ -126,74 +141,98 @@ void Cholesky<Backend::MC, Device::CPU, T>::call_L(comm::CommunicatorGrid grid,
   const comm::Index2D this_rank = grid.rank();
 
   matrix::Distribution const& distr = mat_a.distribution();
-  SizeType nrtile = mat_a.nrTiles().cols();
+  const SizeType nrtile = mat_a.nrTiles().cols();
+
+  constexpr std::size_t N_WORKSPACES = 1;
+  RoundRobin<matrix::Panel<Coord::Col, T, Device::CPU>> panel_cols(N_WORKSPACES, distr);
+  RoundRobin<matrix::Panel<Coord::Row, T, Device::CPU>> panel_cols_t(N_WORKSPACES, distr);
 
   for (SizeType k = 0; k < nrtile; ++k) {
-    // Create a placeholder that will store the shared futures representing the panel
-    std::unordered_map<SizeType, hpx::shared_future<ConstTileType>> panel;
+    const GlobalTileIndex kk_idx(k, k);
+    const comm::Index2D kk_rank = distr.rankGlobalTile(kk_idx);
 
-    GlobalTileIndex kk_idx(k, k);
-    comm::Index2D kk_rank = distr.rankGlobalTile(kk_idx);
+    const LocalTileSize kk_offset{
+        distr.nextLocalTileFromGlobalTile<Coord::Row>(k),
+        distr.nextLocalTileFromGlobalTile<Coord::Col>(k),
+    };
 
-    // Broadcast the diagonal tile along the `k`-th column
-    if (this_rank == kk_rank) {
-      potrf_diag_tile(executor_hp, mat_a(kk_idx));
-      panel[k] = mat_a.read(kk_idx);
-      if (k != nrtile - 1)
-        dataflow(executor_mpi, comm::sendTile_o, mpi_task_chain(), Coord::Col, panel[k]);
-    }
-    else if (this_rank.col() == kk_rank.col()) {
-      if (k != nrtile - 1)
-        panel[k] = dataflow(executor_mpi, comm::recvAllocTile<T>, mpi_task_chain(), Coord::Col,
-                            mat_a.tileSize(kk_idx), kk_rank.row());
-    }
+    const LocalTileIndex diag_wp_idx{0, kk_offset.cols()};
 
-    // Iterate over the k-th column
-    for (SizeType i = k + 1; i < nrtile; ++i) {
-      GlobalTileIndex ik_idx(i, k);
-      comm::Index2D ik_rank = mat_a.rankGlobalTile(ik_idx);
+    auto& panel_col = panel_cols.next_resource();
+    auto& panel_col_t = panel_cols_t.next_resource();
 
-      if (this_rank == ik_rank) {
-        trsm_panel_tile(executor_hp, panel[k], mat_a(ik_idx));
-        panel[i] = mat_a.read(ik_idx);
-        dataflow(executor_mpi, comm::sendTile_o, mpi_task_chain(), Coord::Row, panel[i]);
-      }
-      else if (this_rank.row() == ik_rank.row()) {
-        panel[i] = dataflow(executor_mpi, comm::recvAllocTile<T>, mpi_task_chain(), Coord::Row,
-                            mat_a.tileSize(ik_idx), ik_rank.col());
-      }
-    }
+    panel_col.set_offset(kk_offset);
+    panel_col_t.set_offset(kk_offset);
 
-    // Iterate over the trailing matrix
-    for (SizeType j = k + 1; j < nrtile; ++j) {
-      GlobalTileIndex jj_idx(j, j);
-      comm::Index2D jj_rank = mat_a.rankGlobalTile(jj_idx);
-
-      if (this_rank.col() != jj_rank.col())
-        continue;
-
-      // Broadcast the jk-tile along the j-th column and update the jj-tile
-      if (this_rank.row() == jj_rank.row()) {
-        auto& trailing_matrix_executor = (j == k + 1) ? executor_hp : executor_np;
-        herk_trailing_diag_tile(trailing_matrix_executor, panel[j], mat_a(jj_idx));
-        if (j != nrtile - 1)
-          dataflow(executor_mpi, comm::sendTile_o, mpi_task_chain(), Coord::Col, panel[j]);
-      }
-      else {
-        GlobalTileIndex jk_idx(j, k);
-        if (j != nrtile - 1)
-          panel[j] = dataflow(executor_mpi, comm::recvAllocTile<T>, mpi_task_chain(), Coord::Col,
-                              mat_a.tileSize(jk_idx), jj_rank.row());
-      }
-
-      for (SizeType i = j + 1; i < nrtile; ++i) {
-        // Update the ij-tile using the ik-tile and jk-tile
-        if (this_rank.row() == distr.rankGlobalTile<Coord::Row>(i)) {
-          GlobalTileIndex ij_idx(i, j);
-          gemm_trailing_matrix_tile(executor_np, panel[i], panel[j], mat_a(ij_idx));
+    // Factorization of diagonal tile and broadcast it along the `k-th column
+    if (kk_rank.col() == this_rank.col()) {
+      if (kk_rank.row() == this_rank.row()) {
+        potrf_diag_tile(executor_hp, mat_a(kk_idx));
+        if (k != nrtile - 1) {
+          panel_col_t.set_tile(diag_wp_idx, mat_a.read(kk_idx));
+          dataflow(executor_mpi, comm::sendTile_o, mpi_task_chain(), Coord::Col,
+              panel_col_t.read(diag_wp_idx));
         }
       }
+      else {
+        if (k != nrtile - 1)
+          dataflow(executor_mpi, comm::recvTile_o, mpi_task_chain(), Coord::Col, panel_col_t(diag_wp_idx),
+              kk_rank.row());
+      }
     }
+
+    if (k == nrtile - 1)
+      continue;
+
+    // COLUMN UPDATE
+    for (SizeType i = distr.nextLocalTileFromGlobalTile<Coord::Row>(k + 1);
+         i < distr.localNrTiles().rows(); ++i) {
+      const LocalTileIndex local_idx(Coord::Row, i);
+      const LocalTileIndex ik_idx(i, distr.localTileFromGlobalTile<Coord::Col>(k));
+
+      if (kk_rank.col() == this_rank.col()) {
+        trsm_panel_tile(executor_hp, panel_col_t.read(diag_wp_idx), mat_a(ik_idx));
+
+        panel_col.set_tile(local_idx, mat_a.read(ik_idx));
+      }
+    }
+
+    // row panel has been used for temporary storage of diagonal panel for column update
+    panel_col_t.reset();
+
+    // TODO skip last step tile
+    broadcast(executor_mpi, kk_rank.col(), panel_col, panel_col_t, mpi_task_chain);
+
+    // TRAILING MATRIX
+    for (SizeType jt_idx = k + 1; jt_idx < nrtile; ++jt_idx) {
+      const auto owner = distr.rankGlobalTile({jt_idx, jt_idx});
+
+      if (owner.col() != this_rank.col())
+        continue;
+
+      const auto j = distr.localTileFromGlobalTile<Coord::Col>(jt_idx);
+      auto& trailing_matrix_executor = (j == k + 1) ? executor_hp : executor_np;
+      if (this_rank.row() == owner.row()) {
+        const auto i = distr.localTileFromGlobalTile<Coord::Row>(jt_idx);
+
+        herk_trailing_diag_tile(trailing_matrix_executor, panel_col.read({Coord::Row, i}),
+            mat_a(LocalTileIndex{i, j}));
+      }
+
+      for (SizeType i_idx = jt_idx + 1; i_idx < nrtile; ++i_idx) {
+        const auto owner_row = distr.rankGlobalTile<Coord::Row>(i_idx);
+
+        if (owner_row != this_rank.row())
+          continue;
+
+        const auto i = distr.localTileFromGlobalTile<Coord::Row>(i_idx);
+        gemm_trailing_matrix_tile(executor_np, panel_col.read({Coord::Row, i}),
+                                  panel_col_t.read({Coord::Col, j}), mat_a(LocalTileIndex{i, j}));
+      }
+    }
+
+    panel_col.reset();
+    panel_col_t.reset();
   }
 }
 
@@ -205,7 +244,6 @@ DLAF_CHOLESKY_MC_ETI(extern, float)
 DLAF_CHOLESKY_MC_ETI(extern, double)
 DLAF_CHOLESKY_MC_ETI(extern, std::complex<float>)
 DLAF_CHOLESKY_MC_ETI(extern, std::complex<double>)
-
 }
 }
 }
