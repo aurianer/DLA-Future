5266d24b0c5cd5c1b88c31bc18ad2fb52078a3bb
Alberto Invernizzi <invernizzi@cscs.ch>
2021-03-03 13:08:02 +0100

    add back miniapp in the buildsystem + adapt to hpx 1.6.0

--- miniapp/miniapp_reduction_to_band.cpp
+++ miniapp/miniapp_reduction_to_band.cpp
@@ -61,7 +61,22 @@ int main(int argc, char** argv) {
     ("grid-cols",   value<int>()      ->default_value(1), "Number of column processes in the 2D communicator");
   // clang-format on
 
-  auto ret_code = hpx::init(miniapp, desc_commandline, argc, argv);
+  hpx::init_params p;
+  p.desc_cmdline = desc_commandline;
+  p.rp_callback = [](auto& rp, auto) {
+    int ntasks;
+    DLAF_MPI_CALL(MPI_Comm_size(MPI_COMM_WORLD, &ntasks));
+    // if the user has asked for special thread pools for communication
+    // then set them up
+    if (ntasks > 1) {
+      // Create a thread pool with a single core that we will use for all
+      // communication related tasks
+      rp.create_thread_pool("mpi", hpx::resource::scheduling_policy::local_priority_fifo);
+      rp.add_resource(rp.numa_domains()[0].cores()[0].pus()[0], "mpi");
+    }
+  };
+
+  auto ret_code = hpx::init(miniapp, argc, argv, p);
 
   std::cout << "finished" << std::endl;
 
