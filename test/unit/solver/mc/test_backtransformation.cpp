317d5f1b6d4c41fe3847a89e7aa484e5326194d0
lara <lara.querciagrossa@gmail.com>
2021-03-08 18:02:32 +0100

    start fixing test for distributed version of algorithm

--- test/unit/solver/mc/test_backtransformation.cpp
+++ test/unit/solver/mc/test_backtransformation.cpp
@@ -165,14 +165,21 @@ void testBacktransformationEigenv(SizeType m, SizeType n, SizeType mb, SizeType 
 
 template <class T>
 void testBacktransformationEigenv(comm::CommunicatorGrid grid, SizeType m, SizeType n, SizeType mb, SizeType nb) {
+    //comm::Index2D src_rank_index(std::max(0, grid.size().rows() - 1), std::min(1, grid.size().cols() - 1));
+  comm::Index2D src_rank_index(0, 0);
+
   LocalElementSize sizeC(m, n);
   TileElementSize blockSizeC(mb, nb);
-  Matrix<T, Device::CPU> mat_c(sizeC, blockSizeC);
+  GlobalElementSize szC = globalTestSize(sizeC);
+  Distribution distrC(szC, blockSizeC, grid.size(), grid.rank(), src_rank_index);
+  Matrix<T, Device::CPU> mat_c(std::move(distrC));
   dlaf::matrix::util::set_random(mat_c);
-
+  
   LocalElementSize sizeV(m, m);
   TileElementSize blockSizeV(mb, mb);
-  Matrix<T, Device::CPU> mat_v(sizeV, blockSizeV);
+  GlobalElementSize szV = globalTestSize(sizeV);
+  Distribution distrV(szV, blockSizeV, grid.size(), grid.rank(), src_rank_index);
+  Matrix<T, Device::CPU> mat_v(std::move(distrV));
   dlaf::matrix::util::set_random(mat_v);
 
   int tottaus;
@@ -185,8 +192,8 @@ void testBacktransformationEigenv(comm::CommunicatorGrid grid, SizeType m, SizeT
 
   if (tottaus > 0) {
     // Copy matrices locally
-    auto mat_c_loc = dlaf::matrix::test::all_gather<T>(mat_c, comm_grid);
-    auto mat_v_loc = dlaf::matrix::test::all_gather<T>(mat_v, comm_grid);
+    auto mat_c_loc = dlaf::matrix::test::all_gather<T>(mat_c, grid);
+    auto mat_v_loc = dlaf::matrix::test::all_gather<T>(mat_v, grid);
 
     // Impose orthogonality: Q = I - v tau v^H is orthogonal (Q Q^H = I)
     // leads to tau = [1 + sqrt(1 - vH v taui^2)]/(vH v) for real
@@ -206,9 +213,10 @@ void testBacktransformationEigenv(comm::CommunicatorGrid grid, SizeType m, SizeT
     // use only a panel
     LocalElementSize sizeT(tottaus, tottaus);
     TileElementSize blockSizeT(mb, mb);
-    Matrix<T, Device::CPU> mat_t(sizeT, blockSizeT);
+    GlobalElementSize szT = globalTestSize(sizeT);
+    Distribution distrT(szT, blockSizeT, grid.size(), grid.rank(), src_rank_index);
+    Matrix<T, Device::CPU> mat_t(std::move(distrT));
     set_zero(mat_t);
-    auto mat_t_loc = dlaf::matrix::test::all_gather<T>(mat_t, comm_grid);
 
     MatrixLocal<T> taus({m, 1}, {1, 1});
     for (SizeType i = tottaus - 1; i > -1; --i) {
@@ -229,17 +237,31 @@ void testBacktransformationEigenv(comm::CommunicatorGrid grid, SizeType m, SizeT
                    mat_c_loc.ptr(GlobalElementIndex{i, 0}), mat_c_loc.ld());
     }
 
-    for (SizeType i = mat_t.nrTiles().cols() - 1; i > -1; --i) {
-      const GlobalElementIndex offset{i * mb, i * mb};
-      const GlobalElementIndex tau_offset{i * mb, 0};
-      auto tile_t = mat_t(LocalTileIndex{i, i}).get();
-      auto numcol = tile_t.size().cols();
-      lapack::larft(lapack::Direction::Forward, lapack::StoreV::Columnwise, mat_v.size().rows() - i * mb,
-                    numcol, mat_v_loc.ptr(offset), mat_v_loc.ld(), taus.ptr(tau_offset), tile_t.ptr(),
-                    tile_t.ld());
-    }
 
-    solver::backTransformation<Backend::MC>(mat_c, mat_v, mat_t);
+    const matrix::Distribution& dist_t = mat_t.distribution();
+    for (SizeType i = mat_t.nrTiles().cols() - 1; i > -1; --i) {
+      auto i_rank_row = dist_t.rankGlobalTile<Coord::Row>(i);
+      auto i_rank_col = dist_t.rankGlobalTile<Coord::Col>(i);
+      
+      if ( mat_t.rankIndex().row() == i_rank_row && mat_t.rankIndex().col() == i_rank_col ){
+	std::cout << "i " << i << " rank " << mat_t.rankIndex().row() << ", " << mat_t.rankIndex().col() << std::endl;
+	const GlobalElementIndex offset{i * mb, i * mb};
+	const GlobalElementIndex tau_offset{i * mb, 0};
+	auto i_local_row = dist_t.localTileFromGlobalTile<Coord::Row>(i);
+	auto i_local_col = dist_t.localTileFromGlobalTile<Coord::Col>(i);
+	auto ii = LocalTileIndex(i_local_row, i_local_col);
+	auto tile_t = mat_t(ii).get();
+	auto numcol = tile_t.size().cols();
+	lapack::larft(lapack::Direction::Forward, lapack::StoreV::Columnwise, mat_v.size().rows() - i * mb,
+		      numcol, mat_v_loc.ptr(offset), mat_v_loc.ld(), taus.ptr(tau_offset), tile_t.ptr(),
+		      tile_t.ld());
+      }
+    }
+//    auto mat_t_loc = dlaf::matrix::test::all_gather<T>(mat_t, grid);
+//    if(mat_t.rankIndex().row() == 0 && mat_t.rankIndex().col() == 0)
+//      print(format::numpy{}, "mat_t loc ", mat_t_loc, std::cout);
+
+    solver::backTransformation<Backend::MC>(grid, mat_c, mat_v, mat_t);
 
     auto result = [& dist = mat_c.distribution(),
                    &mat_local = mat_c_loc](const GlobalElementIndex& element) {
@@ -253,20 +275,22 @@ void testBacktransformationEigenv(comm::CommunicatorGrid grid, SizeType m, SizeT
   }
 }
 
-TYPED_TEST(BackTransformationSolverLocalTest, Correctness) {
-  SizeType m, n, mb, nb;
-
-  for (auto sz : sizes) {
-    std::tie(m, n, mb, nb) = sz;
-    testBacktransformationEigenv<TypeParam>(m, n, mb, nb);
-  }
-}
+//TYPED_TEST(BackTransformationSolverLocalTest, Correctness) {
+//  SizeType m, n, mb, nb;
+//
+//  for (auto sz : sizes) {
+//    std::tie(m, n, mb, nb) = sz;
+//    testBacktransformationEigenv<TypeParam>(m, n, mb, nb);
+//  }
+//}
 
 TYPED_TEST(BackTransformationSolverDistributedTest, Correctness) {
   SizeType m, n, mb, nb;
 
-  for (auto sz : sizes) {
-    std::tie(m, n, mb, nb) = sz;
-    testBacktransformationEigenv<TypeParam>(m, n, mb, nb);
+  for (const auto& comm_grid : {this->commGrids()[0]}) {
+    for (auto sz : sizes) {
+      std::tie(m, n, mb, nb) = sz;
+      testBacktransformationEigenv<TypeParam>(comm_grid, m, n, mb, nb);
+    }
   }
 }
