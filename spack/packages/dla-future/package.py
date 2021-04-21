bdde64581524d46efd73c66f93789f9b9e5bc8f5
Mikael Simberg <simberg@cscs.ch>
2021-04-21 10:33:18 +0200

    Add async_mpi dependency to hpx in spack package

--- spack/packages/dla-future/package.py
+++ spack/packages/dla-future/package.py
@@ -25,7 +25,7 @@ class DlaFuture(CMakePackage, CudaPackage):
     depends_on("mpi")
     depends_on("blaspp")
     depends_on("lapackpp")
-    depends_on("hpx cxxstd=14 networking=none")
+    depends_on("hpx cxxstd=14 networking=none +async_mpi")
     depends_on("hpx@1.6.0:")
     depends_on("hpx +cuda", when="+cuda")
 
