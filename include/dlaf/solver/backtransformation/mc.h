ff2a1a0e2315664b01bf478347ee71b7d865c4e4
lara <lara.querciagrossa@gmail.com>
2021-04-30 18:17:35 +0200

    removing last tile

--- include/dlaf/solver/backtransformation/mc.h
+++ include/dlaf/solver/backtransformation/mc.h
@@ -26,6 +26,7 @@
 #include "dlaf/communication/communicator_grid.h"
 #include "dlaf/communication/functions_sync.h"
 #include "dlaf/communication/init.h"
+#include "dlaf/communication/kernels.h"
 #include "dlaf/executors.h"
 #include "dlaf/factorization/qr.h"
 #include "dlaf/lapack_tile.h"
@@ -82,105 +83,78 @@ void BackTransformation<Backend::MC, Device::CPU, T>::call_FC(Matrix<T, Device::
   using hpx::resource::get_thread_pool;
   using hpx::threads::thread_priority;
 
-  parallel_executor executor_hp(&get_thread_pool("default"), thread_priority::high);
-  parallel_executor executor_normal(&get_thread_pool("default"), thread_priority::default_);
+  auto executor_hp = dlaf::getHpExecutor<Backend::MC>();
+  auto executor_np = dlaf::getNpExecutor<Backend::MC>();
 
   const SizeType m = mat_c.nrTiles().rows();
   const SizeType n = mat_c.nrTiles().cols();
-  const SizeType mb = mat_c.blockSize().rows();
-  const SizeType nb = mat_c.blockSize().cols();
-  const SizeType ms = mat_c.size().rows();
-  const SizeType ns = mat_c.size().cols();
+  const SizeType mv = mat_v.nrTiles().rows();
+  const SizeType nv = mat_v.nrTiles().cols();
+  const SizeType mb = mat_v.blockSize().rows();
+  const SizeType nb = mat_v.blockSize().cols();
+  const SizeType ms = mat_v.size().rows();
+  const SizeType ns = mat_v.size().cols();
 
   // Matrix T
   comm::CommunicatorGrid comm_grid(MPI_COMM_WORLD, 1, 1, common::Ordering::ColumnMajor);
   common::Pipeline<comm::CommunicatorGrid> serial_comm(comm_grid);
+
   int tottaus;
-  if (ms < mb || ms == 0 || ns == 0)
+  if (ms < mb || ms == 0 || nv == 0)
     tottaus = 0;
   else
     tottaus = (ms / mb - 1) * mb + ms % mb;
-  
+
   if (tottaus == 0)
     return;
 
   LocalElementSize sizeT(tottaus, tottaus);
   TileElementSize blockSizeT(mb, mb);
   Matrix<T, Device::CPU> mat_t(sizeT, blockSizeT);
-  set_zero(mat_t);
-  
+
   Matrix<T, Device::CPU> mat_vv({mat_v.size().rows(), mb}, mat_v.blockSize());
   Matrix<T, Device::CPU> mat_w({mat_v.size().rows(), mb}, mat_v.blockSize());
   Matrix<T, Device::CPU> mat_w2({mb, mat_c.size().cols()}, mat_c.blockSize());
 
   SizeType last_mb;
-
   if (mat_v.blockSize().cols() == 1) {
     last_mb = 1;
   }
-  else {
+  else if (mat_v.size().cols()) {
     if (mat_v.size().cols() % mat_v.blockSize().cols() == 0)
       last_mb = mat_v.blockSize().cols();
     else
       last_mb = mat_v.size().cols() % mat_v.blockSize().cols();
   }
-  
-  Matrix<T, Device::CPU> mat_vv_last({mat_v.size().rows(), last_mb}, mat_v.blockSize());
-  Matrix<T, Device::CPU> mat_w_last({mat_v.size().rows(), last_mb}, mat_v.blockSize());
-  Matrix<T, Device::CPU> mat_w2_last({last_mb, mat_c.size().cols()}, mat_c.blockSize());
 
-  const SizeType last_reflector_idx = (mat_v.size().cols() < mat_v.size().rows()) ? mat_v.nrTiles().rows() - 2 : mat_v.nrTiles().cols() - 2;
-  
-  for (SizeType k = last_reflector_idx; k > -1; --k) {
+  // Specific for V matrix layout where last column of tiles is empty
+  const SizeType last_reflector_idx = mat_v.nrTiles().cols() - 2;
+
+  for (SizeType k = last_reflector_idx; k >= 0; --k) {
     bool is_last = (k == last_reflector_idx) ? true : false;
 
     for (SizeType i = 0; i < mat_v.nrTiles().rows(); ++i) {
       // Copy V panel into VV
       auto copy_v_into_vv = unwrapping([=](auto&& tile_v, auto&& tile_vv, TileElementSize region) {
-        void (&cpyReg)(TileElementSize, TileElementIndex, const matrix::Tile<const T, Device::CPU>&,
-                       TileElementIndex, const matrix::Tile<T, Device::CPU>&) = copy<T>;
         void (&cpy)(const matrix::Tile<const T, Device::CPU>&, const matrix::Tile<T, Device::CPU>&) =
             copy<T>;
-
-        if (is_last) {
-          TileElementIndex idx_in(0, 0);
-          TileElementIndex idx_out(0, 0);
-          cpyReg(region, idx_in, tile_v, idx_out, tile_vv);
-        }
-        else {
-          cpy(tile_v, tile_vv);
-        }
+        cpy(tile_v, tile_vv);
       });
       hpx::dataflow(executor_hp, copy_v_into_vv, mat_v.read(LocalTileIndex(i, k)),
-                    is_last ? mat_vv_last(LocalTileIndex(i, 0)) : mat_vv(LocalTileIndex(i, 0)),
-                    is_last ? mat_vv_last.tileSize(GlobalTileIndex(i, 0))
-                            : mat_vv.tileSize(GlobalTileIndex(i, 0)));
+                    mat_vv(LocalTileIndex(i, 0)), mat_vv.tileSize(GlobalTileIndex(i, 0)));
 
       // Setting VV
       auto setting_vv = unwrapping([=](auto&& tile) {
-        if (is_last) {
-          if (i <= k) {
-            lapack::laset(lapack::MatrixType::General, tile.size().rows(), tile.size().cols(), 0, 0,
-                          tile.ptr(), tile.ld());
-          }
-          else if (i == k + 1) {
-            lapack::laset(lapack::MatrixType::Upper, tile.size().rows(), tile.size().cols(), 0, 1,
-                          tile.ptr(), tile.ld());
-          }
+        if (i <= k) {
+          lapack::laset(lapack::MatrixType::General, tile.size().rows(), tile.size().cols(), 0, 0,
+                        tile.ptr(), tile.ld());
         }
-        else {
-          if (i <= k) {
-            lapack::laset(lapack::MatrixType::General, tile.size().rows(), tile.size().cols(), 0, 0,
-                          tile.ptr(), tile.ld());
-          }
-          else if (i == k + 1) {
-            lapack::laset(lapack::MatrixType::Upper, tile.size().rows(), tile.size().cols(), 0, 1,
-                          tile.ptr(), tile.ld());
-          }
+        else if (i == k + 1) {
+          lapack::laset(lapack::MatrixType::Upper, tile.size().rows(), tile.size().cols(), 0, 1,
+                        tile.ptr(), tile.ld());
         }
       });
-      hpx::dataflow(executor_hp, setting_vv,
-                    is_last ? mat_vv_last(LocalTileIndex(i, 0)) : mat_vv(LocalTileIndex(i, 0)));
+      hpx::dataflow(executor_hp, setting_vv, mat_vv(LocalTileIndex(i, 0)));
 
       // Copy VV into W
       auto copy_vv_into_w = unwrapping([=](auto&& tile_vv, auto&& tile_w) {
